f10ad1ae1365d8f83239fbbd8c1236390a0da1d5
Anunay Argha <anunayargha@gmail.com>
2024-01-14 23:43:05 +0600

    javaScript start

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+// const displaysContainer = document.querySelector(".display_container__figures");
+
+// let position = 0;
+// const displayAnimation = setInterval(() => {
+// 	if (position === 1536) {
+// 		position = -287;
+// 	} else {
+// 		position++;
+// 		displaysContainer.style.transform = `translate(${position}px)`;
+// 	}
+// }, 1);
